f98b9aea52e7f2293c2e95c1dfee6b3498eab338
eladtzemach <eladtzemach@gmail.com>
2017-10-16 13:12:57 -0400

    added files

---
+++ main.cpp
@@ -0,0 +1,186 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <sstream>
+#include <string>
+#include <iostream>
+#include <opencv\highgui.h>
+#include <opencv\cv.h>
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/core/core.hpp"
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+//initial min and max HSV filter values.
+//these could be changed using trackbars
+int H_MIN = 32;
+int H_MAX = 51;
+int S_MIN = 77;
+int S_MAX = 139;
+int V_MIN = 54;
+int V_MAX = 135;
+//default capture width and height
+const int FRAME_WIDTH = 640;
+const int FRAME_HEIGHT = 480;
+//max number of objects to be detected in frame
+const int MAX_NUM_OBJECTS = 50;
+//minimum and maximum object area
+const int MIN_OBJECT_AREA = 20 * 20;
+const int MAX_OBJECT_AREA = FRAME_HEIGHT*FRAME_WIDTH / 1.5;
+//names that will appear at the top of each window
+const string windowName = "Original Image";
+const string windowName1 = "HSV Image";
+const string windowName2 = "Thresholded Image";
+const string windowName3 = "After Morphological Operations";
+const string trackbarWindowName = "Trackbars";
+void on_trackbar(int, void*)
+{//This function gets called whenever a
+ // trackbar position is changed
+
+}
+string intToString(int number) {
+	std::stringstream ss;
+	ss << number;
+	return ss.str();
+}
+void createTrackbars() {
+	//create window for trackbars
+
+
+	namedWindow(trackbarWindowName, 0);
+	//create memory to store trackbar name on window
+	char TrackbarName[50];
+	sprintf(TrackbarName, "H_MIN", H_MIN);
+	sprintf(TrackbarName, "H_MAX", H_MAX);
+	sprintf(TrackbarName, "S_MIN", S_MIN);
+	sprintf(TrackbarName, "S_MAX", S_MAX);
+	sprintf(TrackbarName, "V_MIN", V_MIN);
+	sprintf(TrackbarName, "V_MAX", V_MAX);
+	//create trackbars and insert them into window
+	//3 parameters are: the address of the variable that is changing when the trackbar is moved,
+	//the max value the trackbar can move, 
+	//and the function that is called whenever the trackbar is moved
+	createTrackbar("H_MIN", trackbarWindowName, &H_MIN, H_MAX, on_trackbar);
+	createTrackbar("H_MAX", trackbarWindowName, &H_MAX, H_MAX, on_trackbar);
+	createTrackbar("S_MIN", trackbarWindowName, &S_MIN, S_MAX, on_trackbar);
+	createTrackbar("S_MAX", trackbarWindowName, &S_MAX, S_MAX, on_trackbar);
+	createTrackbar("V_MIN", trackbarWindowName, &V_MIN, V_MAX, on_trackbar);
+	createTrackbar("V_MAX", trackbarWindowName, &V_MAX, V_MAX, on_trackbar);
+
+
+}
+void drawObject(int x, int y, Mat &frame) {
+
+	//draw a rectangle on tracked image
+
+	rectangle(frame, Point(x-20, y+20), Point(x+20,y-20), Scalar(0, 255, 0), 2);
+
+	//putText(frame, intToString(x) + "," + intToString(y), Point(x, y + 30), 1, 1, Scalar(0, 255, 0), 2);
+
+}
+void morphOps(Mat &thresh) {
+
+	//create structuring element that will be used to "dilate" and "erode" image.
+	Mat erodeElement = getStructuringElement(MORPH_RECT, Size(3, 3));
+	Mat dilateElement = getStructuringElement(MORPH_RECT, Size(8, 8));
+
+	erode(thresh, thresh, erodeElement);
+	erode(thresh, thresh, erodeElement);
+
+	dilate(thresh, thresh, dilateElement);
+	dilate(thresh, thresh, dilateElement);
+
+}
+void trackFilteredObject(int &x, int &y, Mat threshold, Mat &videoFeed) {
+
+	Mat temp;
+	threshold.copyTo(temp);
+	//two vectors needed for output of findContours
+	vector< vector<Point> > contours;
+	vector<Vec4i> hierarchy;
+	//find contours of filtered image using openCV findContours function
+	findContours(temp, contours, hierarchy, CV_RETR_CCOMP, CV_CHAIN_APPROX_SIMPLE);
+	//use moments method to find our filtered object
+	double refArea = 0;
+	bool objectFound = false;
+	if (hierarchy.size() > 0) {
+		int numObjects = hierarchy.size();
+		//if number of objects greater than MAX_NUM_OBJECTS we have a noisy filter
+		if (numObjects<MAX_NUM_OBJECTS) {
+			for (int index = 0; index >= 0; index = hierarchy[index][0]) {
+
+				Moments moment = moments((cv::Mat)contours[index]);
+				double area = moment.m00;
+
+				//if the area is less than 20 px by 20px then it is probably just noise
+				//if the area is the same as the 3/2 of the image size, probably just a bad filter
+				//we only want the object with the largest area so we safe a reference area each
+				//iteration and compare it to the area in the next iteration.
+				if (area>MIN_OBJECT_AREA && area<MAX_OBJECT_AREA && area>refArea) {
+					x = moment.m10 / area;
+					y = moment.m01 / area;
+					objectFound = true;
+					refArea = area;
+				}
+				else objectFound = false;
+
+
+			}
+			if (objectFound == true) {
+				putText(videoFeed, "Tracked Object", Point(0, 50), 2, 1, Scalar(0, 255, 0), 2);
+				//draw object location on screen
+				drawObject(x, y, videoFeed);
+			}
+
+		}
+		else putText(videoFeed, "Too much noise, adjust filter!", Point(0, 50), 1, 2, Scalar(0, 0, 255), 2);
+	}
+}
+int main(int argc, char* argv[])
+{
+	bool trackObjects = true;
+	bool useMorphOps = true;
+	//Matrix to store each frame of the webcam feed
+	Mat videoFeed;
+	//matrix storage for HSV image
+	Mat HSV;
+	//matrix storage for binary threshold image
+	Mat threshold;
+	//x and y values for the location of the object
+	int x = 0, y = 0;
+	//create slider bars for HSV filtering
+	createTrackbars();
+	
+   VideoCapture capture(".../CSI4133/Assignment05/videos/video2.avi");
+
+	//start an infinite loop where frames are copied to videoFeed matrix
+	//all of the operations will be performed within this loop
+	while (1) {
+		//convert frame from BGR to HSV colorspace
+		bool bSuccess = capture.read(videoFeed); // read a new frame from video
+		cvtColor(videoFeed, HSV, COLOR_BGR2HSV);
+		//filter HSV image between values and store filtered image to
+		//threshold matrix
+		inRange(HSV, Scalar(H_MIN, S_MIN, V_MIN), Scalar(H_MAX, S_MAX, V_MAX), threshold);
+		//perform morphological operations on thresholded image to eliminate noise
+		//and emphasize the filtered object(s)
+		if (useMorphOps)
+			morphOps(threshold);
+		//pass in thresholded frame to our object tracking function
+		//this function will return the x and y coordinates of the
+		//filtered object
+		if (trackObjects)
+			trackFilteredObject(x, y, threshold, videoFeed);
+
+		//show frames 
+		imshow(windowName2, threshold);
+		imshow(windowName, videoFeed);
+		imshow(windowName1, HSV);
+
+		//delay 30ms so that screen can refresh.
+		//image will not appear without this waitKey() command
+		waitKey(30);
+	}
+	return 0;
+}
